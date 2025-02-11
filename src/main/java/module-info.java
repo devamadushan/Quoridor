f8edb5ae6f14e9ea96ffcd9be35e86eb62dce5bc
deva <deva.thevaranchan@gmail.com>
2025-02-11 12:59:07 +0100

    Initialisation du projet JavaFX Maven

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.dryt.quoridor {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.dryt.quoridor to javafx.fxml;
+    exports com.dryt.quoridor;
+}
